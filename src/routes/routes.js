d2efbb43677474d84627324c17daa00cadafba9f
hanifxdp <briandglory@gmail.com>
2022-06-24 17:52:49 +0700

    middleware changes 2

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -24,14 +24,19 @@ const upload = multer({ storage });
 const router = Router();
 
 router
-  .get("/budaya", checkLogin(), getBudayaAll)
+  .get("/budaya", checkLogin("user"), getBudayaAll)
   .get("/budaya/all", getAll)
   .get("/budaya/:id", getBudayaById)
   .get("/budaya/list/:id", getListBudaya)
   .get("/budaya/detail/:id", getBudayaDetail)
-  .post("/budaya/add", checkLogin(), upload.single("image"), createBudaya)
-  .patch("/budaya/:id", checkLogin(), upload.single("image"), updateBudaya)
-  .delete("/budaya/:id", checkLogin(), deleteBudaya)
+  .post("/budaya/add", checkLogin("user"), upload.single("image"), createBudaya)
+  .patch(
+    "/budaya/:id",
+    checkLogin("user"),
+    upload.single("image"),
+    updateBudaya
+  )
+  .delete("/budaya/:id", checkLogin("user"), deleteBudaya)
 
   .get("/provinsi/", getProvinsi)
   .get("/provinsi/hitung", countBudaya)
