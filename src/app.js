01504de566b43a45eff2b1fe396a4c52b85522eb
hanifxdp <briandglory@gmail.com>
2022-06-24 13:56:42 +0700

    express plus

--- src/app.js
+++ src/app.js
@@ -27,7 +27,8 @@ const routes = require("./routes/routes");
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded());
+app.use(express.multipart());
 app.use(session(sessionConfig(sessionStore)));
 app.use(passport.initialize());
 app.use(passport.session());
