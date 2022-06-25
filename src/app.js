109a8ddbf984293a12f5bf320363627142fa8a89
hanifxdp <briandglory@gmail.com>
2022-06-25 18:47:11 +0700

    app changes https app

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const budayaRoutes = require("./routes/routes");
 const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const cors = require("cors");
 const fs = require("fs");
-const routes = require("./routes/routes");
-const app = express();
-const passport = require("./utils/passport");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const sessionConfig = require("./config/sessionConfig");
-const { sequelize, connectDB } = require("./utils/database");
 const http = require("http");
 const enforce = require("express-sslify");
+
+const routes = require("./routes/routes");
+
+const passport = require("./utils/passport");
+const sessionConfig = require("./config/sessionConfig");
+const { sequelize, connectDB } = require("./utils/database");
+
+const app = express();
 const port = process.env.PORT || 8000;
 const sessionStore = new SequelizeStore({
   db: sequelize,
@@ -39,17 +42,17 @@ app.use(session(sessionConfig(sessionStore)));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+app.use("/api/v1", budayaRoutes);
+app.use(express.urlencoded({ extended: true }));
+
 const httpServer = http.createServer(app);
 
 connectDB().then(async () => {
   await httpServer.listen(port);
   console.log(`Server is running on port ${port}`);
 });
-
-app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
-});
-app.use("/api/v1", budayaRoutes);
-app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
